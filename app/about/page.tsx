2130e99350f64626eea65a36542fc45cc0d08a1f
aqlhsyfqhsym <nuraqilah.syafiqah@yahoo.com>
2025-01-02 21:08:04 +0800

    make map for same component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,11 @@ import Heading from "@/components/atoms/Heading";
 import Hero from "@/components/organisms/Hero";
 import About from "@/data/about.json";
 import CardImage from "@/components/organisms/CardImage";
+import Subheading from "@/components/atoms/Subheading";
 
+import CorporateProfileFeature from "@/components/molecules/SubTitleParagraph";
 
+ 
 export default function Sustainability() {
   return (
     <div className="max-w-none bg-[#FBFBFB]">
@@ -20,7 +23,19 @@ export default function Sustainability() {
         <section className="py-4 md:py-8">
           <Heading heading={About.ourpurpose.title} className="mb-11" />
           <CardImage cards={About.ourpurpose.image} />
+        </section>
 
+        <section className="py-4 md:py-8">
+          <Heading heading={About.aboutus.title} className="mb-11" />
+          <Subheading subheading={About.aboutus.description} />
+
+          {About.aboutus.features.map((feature, index) => (
+            <CorporateProfileFeature
+              key={index}
+              title={feature.title}
+              description={feature.description}
+            />
+          ))}
         </section>
       </div>
     </div>
