6ac6e907d384241f6ef0f16612649096de068b09
aqlhsyfqhsym <nuraqilah.syafiqah@yahoo.com>
2025-01-02 19:08:50 +0800

    chore: Add subheadingleft prop to hero component

--- app/sustainability/page.tsx
+++ app/sustainability/page.tsx
@@ -8,52 +8,50 @@ import Environmental from "@/components/molecules/SubTitleParagraph";
 import Compliance from "@/components/molecules/SubTitleParagraph";
 
 export default function Sustainability() {
-    return (
-        <div className="max-w-none bg-[#FBFBFB]">
-            <Hero
-                heading={SustainData.hero.heading}
-                subheading={SustainData.hero.subheading}
-                image={SustainData.hero.image}
-            />
-            <Vector />
+  return (
+    <div className="max-w-none bg-[#FBFBFB]">
+      <Hero
+        heading={SustainData.hero.heading}
+        subheading={SustainData.hero.subheading}
+        subheadingleft={SustainData.hero.subheading}
+        image={SustainData.hero.image}
+      />
+      <Vector />
 
-            <div className="container mx-auto p-10 lg:px-48">
-                <section className="py-4 md:py-8">
-                    <Heading heading={SustainData.aboutus.title} className="mb-11" />
-                    <CreditRisk
-                        title={SustainData.creditrisk.title}
-                        description={SustainData.creditrisk.description}
-                    />
+      <div className="container mx-auto p-10 lg:px-48">
+        <section className="py-4 md:py-8">
+          <Heading heading={SustainData.aboutus.title} className="mb-11" />
+          <CreditRisk
+            title={SustainData.creditrisk.title}
+            description={SustainData.creditrisk.description}
+          />
 
-                    <InternalControl
-                        title={SustainData.internalcontrol.title}
-                        description={SustainData.internalcontrol.description}
-                    />
-                    <img
-                        src="/images/sustainability/ship2.png"
-                        alt="Latest Insights stock"
-                        className="w-full object-cover"
-                    />
+          <InternalControl
+            title={SustainData.internalcontrol.title}
+            description={SustainData.internalcontrol.description}
+          />
+          <img
+            src="/images/sustainability/ship2.png"
+            alt="Latest Insights stock"
+            className="w-full object-cover"
+          />
 
-                    <InternalControl
-                        title={SustainData.internalcontrol.title}
-                        description={SustainData.internalcontrol.description}
-                    />
+          <InternalControl
+            title={SustainData.internalcontrol.title}
+            description={SustainData.internalcontrol.description}
+          />
 
-                    <Environmental
-                        title={SustainData.environmental.title}
-                        description={SustainData.environmental.description}
-                    />
+          <Environmental
+            title={SustainData.environmental.title}
+            description={SustainData.environmental.description}
+          />
 
-                    <Compliance
-                        title={SustainData.compliance.title}
-                        description={SustainData.compliance.description}
-                    />
-
-                </section>
-            </div>
-
-
-        </div>
-    );
+          <Compliance
+            title={SustainData.compliance.title}
+            description={SustainData.compliance.description}
+          />
+        </section>
+      </div>
+    </div>
+  );
 }
