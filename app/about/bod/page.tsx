22502b4cd05cbdbba01f68d72e830b04cb21893b
Adam Miller <adam.firshah@gmail.com>
2025-02-12 23:57:18 +0800

    responsive

--- app/about/bod/page.tsx
+++ app/about/bod/page.tsx
@@ -7,8 +7,8 @@ import managementMembers from "@/data/management.json";
 export default function BoardOfDirectors() {
     return (
         <div className="max-w-none bg-[#FBFBFB]">
-            <div className="container mx-auto p-36 lg:px-48">
-                <section className="py-36 md:py-36">
+            <div className="container mx-auto p-10 lg:px-48">
+                <section className="py-10 md:py-36">
                     <Heading heading="Board of Directors" className="mb-11 text-black text-4xl" />
                     <div className="space-y-8">
                         {boardMembers.map((member, index) => (
--- components/atoms/CardManagement/index.tsx
+++ components/atoms/CardManagement/index.tsx
@@ -11,25 +11,25 @@ interface CardManagementProps {
 export default function CardManagement({ image, name, position, description }: CardManagementProps) {
   return (
     <div className="relative border-b border-teal-400/30 pb-8">
-      <div className="flex flex-col md:flex-row gap-8 items-center">
-        <div className="w-full md:w-[380px]">
+      <div className="flex flex-col lg:flex-row gap-8 items-center">
+        <div className="w-full sm:w-full md:w-[500px] lg:w-[420px] mx-auto">
           <div className="relative">
-            <div className="absolute -top-0 -left-0 z-10">
+            <div className="absolute -top-3 -left-3 z-10 hidden lg:block">
               <Image
                 src="/images/bod/vector.png"
                 alt="decoration"
-                width={48}
-                height={48}
-                className="w-12 h-12"
+                width={64}
+                height={64}
+                className="w-16 h-16"
               />
             </div>
             
-            <div className="relative rounded-lg overflow-hidden border-2 border-black">
+            <div className="relative rounded-lg overflow-hidden border-[3px] border-black">
               <Image
                 src={image}
                 alt={name}
-                width={380}
-                height={475}
+                width={500}
+                height={625}
                 className="w-full aspect-[4/5] object-cover"
               />
             </div>
